3e1f69cf3c88835fe224ed960b3121ec73edf7ff
jinwang Yang <1574891843@qq.com>
2024-04-07 23:30:30 +0800

    nn

--- src/Presentation/MauiStockTake.UI/MauiStockTake.UI/AppShell.xaml.cs
+++ src/Presentation/MauiStockTake.UI/MauiStockTake.UI/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MauiStockTake.UI
+﻿using MauiStockTake.UI.Pages;
+
+namespace MauiStockTake.UI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("login", typeof(LoginPage));
         }
     }
 }
