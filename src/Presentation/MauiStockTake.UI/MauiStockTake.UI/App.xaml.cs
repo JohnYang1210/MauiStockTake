7b0379add5ba990d3828f00773baf993ac1b8566
jinwang Yang <1574891843@qq.com>
2024-04-07 21:54:03 +0800

    nn

---
+++ src/Presentation/MauiStockTake.UI/MauiStockTake.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiStockTake.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Presentation/MauiStockTake.UI/MauiStockTake.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiStockTake.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
