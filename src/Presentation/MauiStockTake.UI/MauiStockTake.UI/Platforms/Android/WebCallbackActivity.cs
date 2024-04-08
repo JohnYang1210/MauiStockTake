8716392b35489ad8c6b8d4edee31354310777346
jinwang Yang <1574891843@qq.com>
2024-04-08 23:42:29 +0800

    n

---
+++ src/Presentation/MauiStockTake.UI/MauiStockTake.UI/Platforms/Android/WebCallbackActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiStockTake.UI.Platforms.Android
+{
+    [Activity(NoHistory =true,LaunchMode =LaunchMode.SingleTop,Exported =true)]
+    [IntentFilter(new[] {Intent.ActionNewOutgoingCall})]
+    public class WebCallbackActivity:WebAuthenticatorCallbackActivity
+    {
+    }
+}
