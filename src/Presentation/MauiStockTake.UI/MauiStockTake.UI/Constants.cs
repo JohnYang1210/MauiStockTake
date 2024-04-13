bf90cc3e5a3dedff88a35441b374a800acf7ab7d
jinwang Yang <1574891843@qq.com>
2024-04-13 15:08:13 +0800

    nn

---
+++ src/Presentation/MauiStockTake.UI/MauiStockTake.UI/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiStockTake.UI
+{
+    public static class Constants
+    {
+        public const string BaseUrl = "https:xxxx";
+        public const string RedirectUri = "auth.com.mildredsurf.stocktake://callback";
+    }
+}
